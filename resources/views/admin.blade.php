d1b726df681879b2d68c1e9b562cce2e222c3081
Breyhan-F9 <breyhan20@gmail.com>
2023-01-27 01:40:35 +0700

    sidebar panel kecil

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -11,7 +11,7 @@
 
 <body>
     <section name="navbar">
-        <nav class="bg-white border-gray-200 px- sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <nav class="bg-white border-gray-200 px- sm:px-4 py-1 rounded dark:bg-gray-900">
             <div class="container flex flex-wrap items-center justify-between mx-auto">
                 <div class="px-0 py-0">
                     <img src=' Assets/images/Madukuy RGB Logogram.png'
@@ -101,19 +101,81 @@
     </section>
     </nav>
 
-
-    <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar"
-        type="button"
-        class="inline-flex items-center p-2 ml-3 mt-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-        <span class="sr-only">Open sidebar</span>
-        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path clip-rule="evenodd" fill-rule="evenodd"
-                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
-            </path>
-        </svg>
-    </button>
-
+    <section name="sidebar">
+    <aside id="logo-sidebar"
+        class="w-20 fixed left-0 top-0 h-screen transition-transform-translate-x-full sm:translate-x-0 z-40"
+        aria-label="logo-sidebar">
+        <div class="px-3 py-1  bg-background dark:bg-gray-800 h-full">
+            <div class="flex h-20 w-20 items-center justify-center mt-3 mb-9">
+                <img src=' Assets/images/Madukuy RGB Logogram.png'
+                    class="h-15 w-16  inset-y-0 flex-items-center mr-7" >
+            </div>
+            <ul class=" space-y-2">
+                <li>
+                    <a href="#"
+                        class="flex items-center p-4 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gradient-to-bl from-yellow-200 to-bg-yellow-300">
+                        <svg aria-hidden="true"
+                            class="w-6 h-6 text-black transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                            <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center p-4 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gradient-to-bl from-yellow-200 to-bg-yellow-300 ">
+                        <svg aria-hidden="true"
+                            class="flex-shrink-0 w-6 h-6 text-black transition duration-75 dark:text-gray-400 group-hover:bg-yellow-200"
+                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z">
+                            </path>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center p-4 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gradient-to-bl from-yellow-200 to-bg-yellow-300">
+                        <svg aria-hidden="true"
+                            class="flex-shrink-0 w-6 h-6 text-black transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z">
+                            </path>
+                            <path
+                                d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z">
+                            </path>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center p-4 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gradient-to-bl from-yellow-200 to-bg-yellow-300">
+                        <svg aria-hidden="true"
+                            class="flex-shrink-0 w-6 h-6 text-black transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center p-4 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gradient-to-bl from-yellow-200 to-bg-yellow-300">
+                        <svg aria-hidden="true"
+                            class="flex-shrink-0 w-6 h-6 text-black transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    </aside>
     {{-- <aside id="logo-sidebar"
         class="w-64 fixed left-0 top-0 h-screen transition-transform -translate-x-full sm:translate-x-0 z-40"
         aria-label="Sidebar">
