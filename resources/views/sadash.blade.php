e09fa0d6708f4376233c25f96bf645729ee4ca0d
Fikkie007 <fikkieakbar48@gmail.com>
2023-01-30 00:07:17 +0700

    fix minor error in table

--- resources/views/sadash.blade.php
+++ resources/views/sadash.blade.php
@@ -433,7 +433,7 @@
                     </th>
                     <td class="mt-2 px-4 align-center">
                         <button disabled type="button"
-                            class="text-white bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 text-xs font-medium rounded-full text-sm px-1 py-0.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">In
+                            class="text-white bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 text-xs font-medium rounded-full px-1 py-0.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">In
                             Progress</button>
 
                     </td>
@@ -479,7 +479,7 @@
                     </th>
                     <td class="mt-2 px-4 align-center">
                         <button disabled type="button"
-                            class="text-white bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 text-xs font-medium rounded-full text-sm px-1 py-0.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">In
+                            class="text-white bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 text-xs font-medium rounded-full px-1 py-0.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">In
                             Progress</button>
 
                     </td>
@@ -525,7 +525,7 @@
                     </th>
                     <td class="mt-2 px-4 align-center">
                         <button disabled type="button"
-                            class="text-white bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 text-xs font-medium rounded-full text-sm px-1 py-0.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">In
+                            class="text-white bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 text-xs font-medium rounded-full px-1 py-0.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">In
                             Progress</button>
 
                     </td>
